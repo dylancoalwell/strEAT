49353f80ff7b22a423dc36322795fe109a075762
Dylan Coalwell <email": null>
2016-09-02 16:58:47 -0500

    User successfully registered.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new(user_params)
-    if @user.save
+  end
+
+  def create
+    @user = User.create!(user_params)
+    if @user.id
       redirect_to root_path
     else
       render 'users#new'
@@ -14,6 +17,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :phone, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions
+
+  post '/users' => 'users#create'
 end
