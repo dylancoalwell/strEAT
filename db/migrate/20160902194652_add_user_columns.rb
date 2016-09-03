5715465e4e78baaa3d284bfaa6001ce0168cc47d
Dylan Coalwell <email": null>
2016-09-02 19:18:54 -0500

    got rid of duplicate migration

--- db/migrate/20160902194652_add_user_columns.rb
+++
@@ -1,9 +0,0 @@
-class AddUserColumns < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :email, :string
-    add_column :users, :phone, :string
-    add_column :users, :password_digest, :string
-  end
-end
