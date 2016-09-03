8a8d63e7d6aa849f9880941406125da33bef4817
Dylan Coalwell <email": null>
2016-09-03 18:46:46 -0500

    added validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_secure_password
   popular
 
+  validates :first_name, :last_name, :email, :phone, :password, presence: true
+  validates :email, uniqueness: true
+
   def self.search(search)
     where("phone LIKE ?", "%#{search}%")
   end
