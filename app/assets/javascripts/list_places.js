7e1236c4995a63a991fc64827630d758a1009ca3
Dylan Coalwell <email": null>
2016-09-07 15:59:11 -0500

    links on the list are now linking to google maps directions.

--- app/assets/javascripts/list_places.js
+++ app/assets/javascripts/list_places.js
@@ -14,6 +14,16 @@ function showList(list) {
   $('#list-button').on('click', function(e) {
     var newList = removeDuplicatesFromList(list);
     e.preventDefault();
-    console.log(newList.length);
+    placeList(newList);
+  });
+};
+
+function placeList(list) {
+  $('#map').slideUp(1000);
+  list.forEach(function(singleItem) {
+    var address = encodeURIComponent(singleItem.vicinity)
+    var mapsUrl = "http://maps.google.com/maps?dirflg=w&saddr="+currentPosition.coords.latitude+","+currentPosition.coords.longitude+"&daddr="+address+"&dirflg=w"
+    var listHtml = "<li class='card-title'><a style='{color: red}'href="+mapsUrl+">"+singleItem.name+"</a></li></li>"+singleItem.vicinity+"</li><br>"
+    $('#restaurant-list').append(listHtml);
   });
 };
