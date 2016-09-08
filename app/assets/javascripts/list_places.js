88cf844a111457b163b25472621e07361a97f587
Dylan Coalwell <email": null>
2016-09-08 09:52:50 -0500

    pretty sure the duplicate bug is now fixed.

--- app/assets/javascripts/list_places.js
+++ app/assets/javascripts/list_places.js
@@ -2,7 +2,7 @@
 function removeDuplicatesFromList(list) {
   for ( var i = 0; i < list.length; i++ ) {
     for ( var j = 0; j < list.length; j++ ) {
-      if(list[i].place_id == list[j].place_id) {
+      if(list[i].place_id == list[j].place_id && list.indexOf(list[i]) != list.indexOf(list[j])) {
         list.splice(j, 1);
       }
     }
@@ -40,4 +40,4 @@ function appendListHtml(item) {
   var mapsUrl = "http://maps.google.com/maps?dirflg=w&saddr="+currentPosition.coords.latitude+","+currentPosition.coords.longitude+"&daddr="+address+"&dirflg=w"
   var listHtml = "<li class='card-title'><a href="+mapsUrl+">"+item.name+"</a></li><li>"+item.vicinity+"</li><li>"+coolHtml+"</li><br>"
   $('#restaurant-list').append(listHtml);
-}
+};
